d1e3873976d843745d7c04d83c7c3d3b200021ff
0lsookyeong <51187906+0lsookyeong@users.noreply.github.com>
2023-02-11 20:17:06 +0900

    todo using vue first
    입력,삭제 시 바로 데이터 화면에  반영되지 않는 것까지 짠 코드

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <todoHeader></todoHeader>
+    <todoInput></todoInput>
+    <todoList></todoList>
+    <todoFooter></todoFooter>
+  </div>
+</template>
+
+<script>
+
+  import todoHeader  from './components/todoHeader.vue'
+  import todoInput  from './components/todoInput.vue'
+  import todoList  from './components/todoList.vue'
+  import todoFooter  from './components/todoFooter.vue'
+
+export default {
+  components: {
+    'todoHeader': todoHeader,
+    'todoInput': todoInput,
+    'todoList': todoList,
+    'todoFooter': todoFooter
+  }
+}
+</script>
+
+<style>
+   body {
+    text-align: center;
+    background-color: #F6F6F8;
+  }
+  input {
+    border-style: groove;
+    width: 200px;
+  }
+  button {
+    border-style: groove;
+  }
+  .shadow {
+    box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03)
+  }           
+</style>
---
+++ src/components/todoHeader.vue
@@ -0,0 +1,21 @@
+<template>
+	<header>
+		<h1>To Do !!</h1>
+	</header>
+
+</template>
+
+<script type="text/javascript">
+export default{
+
+	
+}	
+</script>
+<!-- scoped 은 vue 에서만 지원하는 속성 , 스타일 정의를 해당 컴포넌트에만 적용하겠다는 의미  -->
+<style scoped>
+  h1 {
+    color: #2F3B52;
+    font-weight: 900;
+    margin: 2.5rem 0 1.5rem;
+  }
+</style>
